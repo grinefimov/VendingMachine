b7b996c381263dd3de077d4c34d5aa5b4308b523
Grin <grinefimov@yandex.ru>
2019-09-25 16:34:58 +0300

    Add AdminController

--- VendingMachine/Models/Product.cs
+++ VendingMachine/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace VendingMachine.Models
         public int Id { get; set; }
         [Required] public string Name { get; set; }
         [Required] public float Price { get; set; }
-        [Required] public string ImageUrl { get; set; }
-        [Required] public string Quantity { get; set; }
+        [Required] public int Quantity { get; set; }
+        [Required]
+        [DisplayName("Image URL")]
+        public string ImageUrl { get; set; }
     }
 }
