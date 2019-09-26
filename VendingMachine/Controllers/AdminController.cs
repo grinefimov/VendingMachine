864ad9b75c51e859466d23144580ad4820cec6bb
Grin <grinefimov@yandex.ru>
2019-09-26 23:07:58 +0300

    Add DeleteProduct

--- VendingMachine/Controllers/AdminController.cs
+++ VendingMachine/Controllers/AdminController.cs
@@ -105,83 +105,9 @@ namespace VendingMachine.Controllers
                 model.NewProduct.ImageUrl = @"/images/" + newFilename;
             }
 
-            if (true)
-            {
-                _context.Add(model.NewProduct);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-
-            }
-
+            _context.Add(model.NewProduct);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        // GET: Admin/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Admin/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,ImageUrl,Quantity")] Product product, IFormFile file)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
-
-            if (file != null)
-            {
-                var deletePath = Path.Combine("", _hostingEnvironment.ContentRootPath + @"/wwwroot/" + product.ImageUrl);
-                if (System.IO.File.Exists(deletePath))
-                {
-                    System.IO.File.Delete(deletePath);
-                }
-                var fileInfo = new FileInfo(file.FileName);
-                var newFilename = product.Name + fileInfo.Extension;
-                var path = Path.Combine("", _hostingEnvironment.ContentRootPath + @"\wwwroot\images\" + newFilename);
-                await using (var stream = new FileStream(path, FileMode.Create))
-                {
-                    await file.CopyToAsync(stream);
-                }
-                product.ImageUrl = @"/images/" + newFilename;
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
         }
 
         // GET: Admin/Delete/5
@@ -203,9 +129,9 @@ namespace VendingMachine.Controllers
         }
 
         // POST: Admin/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
             _context.Products.Remove(product);
